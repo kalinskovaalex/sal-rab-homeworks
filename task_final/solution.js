c8927d3a7e77025cdc51d84fc885f1fa8cc5247d
Sasha Voropanova <kalinskovaalex@mail.ru>
2022-11-26 10:45:06 +0300

    task_final_q

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,21 +33,29 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+let client = '';
+let goods = [
+    {title: "Пицца", count: 1},
+    {title: "Чебурек", count: 1}
+];
+let order = {address: "ул. Ленина, дом 24, подъезд, 5 этаж, кв 53", sum: 900};
 
-    let countOfGoods = goods.length;
+function sendRequest() {
+    let data = {client, order, goods};
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    //let countOfGoods = goods.length;
+
+       for (let i = 0; i < goods.length; i++) {
+        data.goods.push({title:goods[i].title, count:goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = order.address;
+    data.order.sum = order.sum;
 
-    data.client = 'Иван';
-
+    data.client = 'Иван +7(987)65-43-210';
+   
     let jsonData = JSON.stringify(data);
 
     return jsonData;
 }
+
